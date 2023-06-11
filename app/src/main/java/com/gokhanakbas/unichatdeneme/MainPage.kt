e1dc746e2b920ba3ba7855c232ab01f807f4b11d
Gökhan Akbaş <gkhn.akbs5050@gmail.com>
2023-06-12 02:04:22 +0300

    Mainpage.kt and activity_mainpage.xml was merged

--- app/src/main/java/com/gokhanakbas/unichatdeneme/MainPage.kt
+++ app/src/main/java/com/gokhanakbas/unichatdeneme/MainPage.kt
@@ -1,4 +1,19 @@
 package com.gokhanakbas.unichatdeneme
 
-class MainPage {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gokhanakbas.unichatdeneme.databinding.ActivityMainpageBinding
+
+class MainPage : AppCompatActivity() {
+    private lateinit var  binding : ActivityMainpageBinding
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainpageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setContentView(R. layout.activity_mainpage)
+    }
+
+
+
 }
