a2294bb58c00a41133e88b28c2f3009cfdcf19bc
Gökhan Akbaş <gkhn.akbs5050@gmail.com>
2023-06-12 02:51:27 +0300

    Recycler Adapter binding problem solved.Recycler row connected to Recycler Adapter.

---
+++ app/src/main/java/com/gokhanakbas/unichatdeneme/RecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.gokhanakbas.unichatdeneme
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gokhanakbas.unichatdeneme.databinding.RecyclerViewItemBinding
+import java.util.ArrayList
+
+class RecyclerAdapter(val postlist : ArrayList<Int>) : RecyclerView.Adapter<RecyclerAdapter.PostsVH>() {
+    //Burada post list i postlarin id sini yazariz diye dusundum.
+    private lateinit var binding : RecyclerViewItemBinding
+    class PostsVH(val itemBinding: RecyclerViewItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsVH {
+        val itemBinding= RecyclerViewItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        // Burada satir goruntusunu baglamis olduk.
+        return PostsVH(itemBinding)
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: PostsVH, position: Int) {
+
+        holder.itemBinding
+    }
+
+}
