abb24ae8dce114ad000a546545238455a58d7b3d
Gökhan Akbaş <gkhn.akbs5050@gmail.com>
2023-06-13 09:32:06 +0300

    login function problem solved.Login processes is done

--- app/src/main/java/com/gokhanakbas/unichatdeneme/EntryPage.kt
+++ app/src/main/java/com/gokhanakbas/unichatdeneme/EntryPage.kt
@@ -21,37 +21,40 @@ class EntryPage : AppCompatActivity() {
         binding=ActivityEntrypageBinding.inflate(layoutInflater)
         val view =binding.root
         setContentView(view)
+
         auth=FirebaseAuth.getInstance()
+
         val guncelKullanici=auth.currentUser
         if(guncelKullanici!=null){
-            val intent = Intent(applicationContext, MainPage::class.java)
+            val intent = Intent(this,MainPage::class.java)
             intent.putExtra("user_mail",auth.currentUser?.email.toString())
             startActivity(intent)
             finish()
         }
 
 
-        setContentView(R.layout.activity_entrypage)
+
     }
 
     fun login(view : View){
-        var user_mailText : String =binding.usermailText.text.toString().trim()
-        var passwordText : String =binding.passwordText.text.toString().trim()
+        var user_mailText =binding.usermailText.text.toString()
+        var passwordText =binding.passwordText.text.toString()
+        println(user_mailText +"<-  Helloo ->" +passwordText)
 
-        //if (!user_mailText.isEmpty() && !passwordText.isEmpty()){
+        if (user_mailText != "" && passwordText != ""){
             auth.signInWithEmailAndPassword(user_mailText,passwordText).addOnCompleteListener {
-                    val guncelKullanici=auth.currentUser?.email.toString()
-                    val intent = Intent(applicationContext, MainPage::class.java)
+                    //val guncelKullanici=auth.currentUser?.email.toString()
+                    val intent = Intent(this,MainPage::class.java)
                     intent.putExtra("user_mail",user_mailText)
                     startActivity(intent)
                     finish()
                 }.addOnFailureListener{
                     Toast.makeText(this.applicationContext,it.localizedMessage,Toast.LENGTH_LONG).show()
             }
-        /*}
+        }
         else{
             Toast.makeText(this.applicationContext,"Bos Alanlar Var",Toast.LENGTH_LONG).show()
-        }*/
+        }
 
 
     }
