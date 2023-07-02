631895ddace4ca4a1e198d0eac9c08552e601599
Gökhan Akbaş <gkhn.akbs5050@gmail.com>
2023-07-02 11:23:15 +0300

    Recycler view item was edited

--- app/src/main/java/com/gokhanakbas/unichatdeneme/pages/User_Profile_Page.kt
+++ app/src/main/java/com/gokhanakbas/unichatdeneme/pages/User_Profile_Page.kt
@@ -11,18 +11,24 @@ import com.gokhanakbas.unichatdeneme.fragments.LikedPostFragmentDirections
 
 class User_Profile_Page : AppCompatActivity() {
     private lateinit var binding : ActivityUserProfilePageBinding
+    private var fragmentStatus : Int =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_page)
         binding=ActivityUserProfilePageBinding.inflate(layoutInflater)
     }
     fun posts_button(view : View){
-
+        if(fragmentStatus!=0){
         val action=PostListFragmentDirections.actionPostListFragment2ToLikedPostFragment2()
         Navigation.findNavController(view).navigate(action)
+            fragmentStatus=0
+            }
     }
     fun liked_button(view: View){
+        if(fragmentStatus!=1){
         val action=LikedPostFragmentDirections.actionLikedPostFragment2ToPostListFragment2()
         Navigation.findNavController(view).navigate(action)
+        fragmentStatus=0
+        }
     }
 }
